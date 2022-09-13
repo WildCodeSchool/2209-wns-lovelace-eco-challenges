33a16c1ceb5758e076183688a192754408825ce8
Arnaud Renaud <arnaud.renaud@gmail.com>
2022-09-13 14:31:33 +0200

    Handle bad user input in wilder creation

--- src/controllers/wilders.js
+++ src/controllers/wilders.js
@@ -7,8 +7,12 @@ const get = async (req, res) => {
 
 const post = async (req, res) => {
   const { firstName, lastName } = req.body;
-  const newWilder = await createWilder(firstName, lastName);
-  res.json(newWilder);
+  if (!firstName || !lastName) {
+    res.status(400).json({ error: "First name and last name are mandatory." });
+  } else {
+    const newWilder = await createWilder(firstName, lastName);
+    res.status(201).json(newWilder);
+  }
 };
 
 module.exports = {
