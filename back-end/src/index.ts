79f6f557ee6838f9515dddf7684728fe211f61fb
Arnaud Renaud <arnaud.renaud@gmail.com>
2022-10-18 11:18:23 +0200

    Replace Express server by dummy Apollo server

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,27 +1,75 @@
-import express from "express";
+import { ApolloServer, gql } from "apollo-server";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
 
-import * as wildersControllers from "./controllers/wilders";
 import SchoolRepository from "./models/School/School.repository";
 import SkillRepository from "./models/Skill/Skill.repository";
 import WilderRepository from "./models/Wilder/Wilder.repository";
 
-const app = express();
-app.use(express.json());
+// const WILDERS_PATH = "/wilders";
+// app.get(WILDERS_PATH, wildersControllers.get);
+// app.post(WILDERS_PATH, wildersControllers.post);
+// app.put(`${WILDERS_PATH}/:id`, wildersControllers.put);
+// app.delete(`${WILDERS_PATH}/:id`, wildersControllers.del);
+// app.post(`${WILDERS_PATH}/:id/skills`, wildersControllers.addSkill);
 
-app.get("/", function (req, res) {
-  res.send("Hello world from Express!");
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  csrfPrevention: true,
+  cache: "bounded",
+  /**
+   * What's up with this embed: true option?
+   * These are our recommended settings for using AS;
+   * they aren't the defaults in AS3 for backwards-compatibility reasons but
+   * will be the defaults in AS4. For production environments, use
+   * ApolloServerPluginLandingPageProductionDefault instead.
+   **/
+  plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
 });
 
-const WILDERS_PATH = "/wilders";
-app.get(WILDERS_PATH, wildersControllers.get);
-app.post(WILDERS_PATH, wildersControllers.post);
-app.put(`${WILDERS_PATH}/:id`, wildersControllers.put);
-app.delete(`${WILDERS_PATH}/:id`, wildersControllers.del);
-app.post(`${WILDERS_PATH}/:id/skills`, wildersControllers.addSkill);
-
-const PORT = 4000;
-
-async function start() {
+// The `listen` method launches a web server.
+server.listen().then(async ({ url }) => {
   await SkillRepository.initializeRepository();
   await SchoolRepository.initializeRepository();
   await WilderRepository.initializeRepository();
@@ -29,9 +77,6 @@ async function start() {
   await SkillRepository.initializeSkills();
   await SchoolRepository.initializeSchools();
   await WilderRepository.initializeWilders();
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT} 👍`);
-  });
-}
 
-start();
+  console.log(`🚀  Server ready at ${url}`);
+});
