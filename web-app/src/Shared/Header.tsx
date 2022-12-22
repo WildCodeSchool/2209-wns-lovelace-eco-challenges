78588e189c826c58a1d0f15d80d9b3303eba7ccc
Marco Da Silva <marco.dasilva@omnilog.fr>
2022-12-22 11:23:17 +0100

    wip header

--- web-app/src/Shared/Header.tsx
+++ web-app/src/Shared/Header.tsx
@@ -3,6 +3,8 @@ const Header = () => {
 
     return (
         <header className="header">
+            <h1 className="title">Eco Playground</h1>
+            <img src="" alt="menu" />
         </header>
     )
 }
